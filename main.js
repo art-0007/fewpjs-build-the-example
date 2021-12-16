9966236c17baa1ed8cffefdf74fecc20e4bec581
Artur <info@aigroupusa.com>
2021-12-16 18:26:29 -0500

    commit tree 562383e7eda96635e52e5dfe6dfadb5a82e709b3

--- main.js
+++ main.js
@@ -3,9 +3,40 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector("#modal")
+const likeBtn = document.querySelector("#\\32 01811189 > footer > ul > li > span")
 
+function heartChange() {
+  if (likeBtn.className === "like-glyph") {
+    likeBtn.innerHTML = FULL_HEART
+    likeBtn.className = "activated-heart"
+  } else if (likeBtn.className === "activated-heart") {
+    likeBtn.innerHTML = EMPTY_HEART
+    likeBtn.className = "like-glyph"
+  }
+}
 
+function setHidden() {
+  modal.className = ('hidden')
+}
+setHidden()
 
+likeBtn.addEventListener('click', addLike )
+
+function addLike() {
+  
+    mimicServerCall()
+  .then(() => { heartChange() })
+  .catch(function(error) {
+    modal.style.visibility = "visible"
+    alert(error.message)
+    setTimeout(setHidden, 3000)
+    function setHidden() {
+      modal.className = ('hidden')
+    }
+    setHidden()
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
